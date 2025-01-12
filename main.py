086089851d57ba9201c7e703b33dbd7830802a3e
Marcos Daniel <marcosnumber11@gmail.com>
2025-01-12 20:36:32 -0300

    (WIP) Don't work for some PLs. 😥

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import numpy as np
 import argparse
 
+# Exceptions for the simplex algorithm
+class Infeasible(Exception):
+    def __init__(self):
+        super().__init__()
+
+class Unbounded(Exception):
+    def __init__(self):
+        super().__init__()
+
 def makeMatrixFullRank(A):
     ''' Esta função recebe uma matriz, que pode ser em numpy,
         e retorna dois argumentos:
@@ -27,7 +36,11 @@ def makeMatrixFullRank(A):
     return A, rowsEliminated
 # end makeMatrixFullRank
 
-def read_simplex_input(file_path : str):
+policy = "bland"
+width = 7
+decimals = 3
+
+def readInput(file_path : str):
     with open(file_path, 'r') as f:
         lines = f.readlines()
 
@@ -53,62 +66,67 @@ def read_simplex_input(file_path : str):
         
     A = np.array(A)
     b = np.array(b)
-    return A, b, c, non_neg, signs
+    return A, b, c, non_neg, signs, num_vars, num_constraints
 
 def formatValue(value, width, decimals): return f"{value:>{width}.{decimals}f}"
 
-def printTableau(A, b, c, valopt, M, M_c, width=6, decimals=2):
+def printTableau(A, b, c, valopt, M, M_c, basis, msg=""):
     def formatRow(row): return " ".join(formatValue(x, width, decimals) for x in row)
 
-    print(formatRow(M_c), '||', formatRow(c), '||', formatValue(valopt, width, decimals))
+    if msg != '': print("\n" + msg, end='\n\n')
+    
+    print(formatRow(M_c), '||', formatRow(c), '||', formatValue(valopt, width, decimals), "  Base: ")
     print('-' * (width * (A.shape[1] + M.shape[1] + 2) + 10))  # Separator line
 
     for i in range(A.shape[0]):
         print(
             formatRow(M[i]), '||',
             formatRow(A[i]), '||',
-            formatValue(b[i], width, decimals)
+            formatValue(b[i], width, decimals),
+            f"      {basis[i]}" if basis[i] != -1 else ""
         )
     print('-' * (width * (A.shape[1] + M.shape[1] + 2) + 10))  
 
-def getFPI():
-    ''' Retorna o problema de programação linear: (LOG 09) '''
+def printResult(status, valopt, sol: list, dual: list):
     
-    A = np.array([[-1.00,  1.00,  1.00,  2.00,  1.00,  0.00,  0.00,  0.00,  0.00,  0.00],
-                  [0.00,  0.00,  1.00, -2.00,  0.00,  1.00,  0.00,  0.00,  0.00,  0.00],
-                  [2.00, -2.00,  1.00,  1.00,  0.00,  0.00,  1.00,  0.00,  0.00,  0.00],
-                  [1.00,  1.00,  0.00, -1.00,  0.00,  0.00,  0.00, -1.00,  0.00,  0.00],
-                  [2.00, -1.00, -1.00,  2.00,  0.00,  0.00,  0.00,  0.00,  1.00,  0.00],
-                  [-1.00, -2.00,  0.00, -2.00,  0.00,  0.00,  0.00,  0.00,  0.00,  1.00]])
-    b = np.array([5.00, 1.00, 9.00, 1.00, 4.00, 9.00])
-    c = np.array([-6.00, 10.00, 1.00, 2.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00])
-    valopt = 0.0
-    M = np.array([[1.00,  0.00,  0.00,  0.00,  0.00,  0.00],
-                 [0.00,  1.00,  0.00,  0.00,  0.00,  0.00],
-                 [0.00,  0.00,  1.00,  0.00,  0.00,  0.00],
-                 [0.00,  0.00,  0.00, -1.00,  0.00,  0.00],
-                 [0.00,  0.00,  0.00,  0.00,  1.00,  0.00],
-                 [0.00,  0.00,  0.00,  0.00,  0.00,  1.00]])
-    M_c = np.array([0.00, 0.00, 0.00, 0.00, 0.00, 0.00])
-    
-    return A, b, c, valopt, M, M_c
+    if status == "otima" or status == "multipla":    
+        print(f"Status: otimo {"(multiplos)" if status == 'multipla' else ''}")
+        print("Objetivo:", valopt)
+        print("Solução: ")
+        for i in sol:
+            print(formatValue(i, width, decimals), end=' ')
+        print()
+        
+        print("Dual: ")
+        for i in dual:
+            print(formatValue(i, width, decimals), end=' ')    
+        print()
+    else:
+        print("Status:", status)
 
-policy = "bland"
+threshold = 1e-6
+# Tratar mal condicionamento das matrizes
+def handleZero(G):
+    G[np.abs(G) < threshold] = 0
+    return G
 
-def find_pivot_column(c):
+
+def findPivotColumn(c):
     ''' Retorna a coluna pivô, caso não exista retorna -1
     '''
-    negative_indices = [i for i, val in enumerate(c) if val < 0]
+    c = handleZero(c)
+    negative_indices = [(val, i) for i, val in enumerate(c) if (val < 0) and (abs(val) > threshold)]
+    
     if not negative_indices:
-        return -1  # Ótimo
+        return -1
     if policy == "bland":
-        return negative_indices[0]
+        negative_indices.sort()
+        return negative_indices[-1][1]
     elif policy == "largest":
-        return max(negative_indices, key=lambda i: abs(c[i]))
+        return np.argmax(c)
     elif policy == "smallest":
-        return min(negative_indices, key=lambda i: abs(c[i]))
+        return np.argmin(c)
     raise ValueError(f"Política desconhecida: {policy}")
-
 
 def pivot(A, b, c_aux, val_opt, M, M_c, row, column):
     ''' Faz o pivoteamento de uma linha e coluna '''
@@ -131,29 +149,18 @@ def pivot(A, b, c_aux, val_opt, M, M_c, row, column):
     val_opt += mult * b[row]
     M_c += mult * M[row]
     
+    
+    A = handleZero(A)
+    b = handleZero(b)
+    c_aux = handleZero(c_aux)
+    M = handleZero(M)
+    M_c = handleZero(M_c)
+    
     return val_opt
-    
-def canonicalForm(basis : list[tuple[int, int]], A, b, c_aux, val_opt, M, M_c):
-    ''' Transforma o tableau para a forma canônica '''
-    for row, column in enumerate(basis):
-        if c_aux[column] != 0:
-            
-            mult = 0
-            mult = -c_aux[column]/A[row][column]
-            
-            line = A[row] * mult
-            c_aux += line
-            val_opt += b[row] * mult
-            
-            line = M[row] * mult
-            M_c += line
-    
-    return val_opt   
 
 def simplexIteration(basis, A_aux, b, c_aux, val_opt, M, M_c):
     
-    # Estratégia de escolha de coluna: escolhe a primeira coluna com elemento negativo -> Bland
-    column = find_pivot_column(c_aux)
+    column = findPivotColumn(c_aux)
     
     # Escolha de qual coluna sai da base (menor razão positiva)
     row = -1
@@ -166,49 +173,49 @@ def simplexIteration(basis, A_aux, b, c_aux, val_opt, M, M_c):
                 row = i
 
     if row == -1:
-        raise Exception('Problema ilimitado')
+        raise Unbounded()
 
     basis[row] = column
 
     return pivot(A_aux, b, c_aux, val_opt, M, M_c, row, column)
     
-def first_phase(A, b, c, M, M_c):
+def firstPhase(basis, A, b, M, M_c):
     ''' Implementação da primeira fase do simplex '''
-    I = np.eye(len(A))
-    A_aux = A.copy()
-    A_aux = np.concatenate((A_aux, I), axis=1)
-
-    c_aux = np.concatenate((np.zeros(len(c)), np.ones(len(I))), axis=0)
+   
+    count = 0
+    c = np.zeros(len(A[0]))
+    for i, bi in enumerate(basis):
+        if bi == -1:
+            A = np.insert(A, A.shape[1], 0, axis=1)
+            A[i][-1] = 1
+            c = np.insert(c, c.shape[0], 1)
+            count += 1
+            basis[i] = len(A[0]) - 1
+            
     val_opt = 0
     
     
-    printTableau(A_aux, b, c_aux, 0, M, M_c)
+    printTableau(A, b, c, val_opt, M, M_c, basis, "Tableau inicial para a PL auxiliar: ")
     
-    # Transform to canonical form
-    # elements in -c^t above basis must be 0
-
-    basis = [len(A[0]) + i for i in range(len(A))]
-
-    printTableau(A_aux, b, c_aux, val_opt, M, M_c)
-    val_opt = canonicalForm(basis, A_aux, b, c_aux, val_opt, M, M_c)            
-    printTableau(A_aux, b, c_aux, val_opt, M, M_c)
+    for i, bi in enumerate(basis):
+        val_opt = pivot(A, b, c, val_opt, M, M_c, i, bi)      
+    
+    printTableau(A, b, c, val_opt, M, M_c, basis, "Forma canônica para a PL auxiliar: ")
     
     curIter = 0
     
-    while np.any(c_aux < 0):
+    while np.any(c < 0):
         curIter += 1
-        val_opt = simplexIteration(basis, A_aux, b, c_aux, val_opt, M, M_c)
-        print(f"Iteração {curIter}")
-        printTableau(A_aux, b, c_aux, val_opt, M, M_c)
-        print("basis:", basis)
+        val_opt = simplexIteration(basis, A, b, c, val_opt, M, M_c)
+        printTableau(A, b, c, val_opt, M, M_c, basis, f"Iteração {curIter}: ")
 
     if val_opt < 0: 
-        raise Exception('PL inviável!')
+        raise Infeasible()
 
 
     # Get the solution 
     
-    sol = [0 for _ in range(len(c_aux))]
+    sol = [0 for _ in range(len(c))]
     for row, col in enumerate(basis):
         sol[col] = b[row]
 
@@ -218,14 +225,23 @@ def first_phase(A, b, c, M, M_c):
         print(f"{i:.3f}", end=' ')
     print()
     
-    return basis, A_aux, b, val_opt, M, M_c
-    
+    A = A[:, :(A.shape[1] - count)]
+    return basis, A, b, M, M_c 
 
-def findInitialbasis(A, b, c, M, M_c):
-    ''' Encontra uma base inicial para o problema '''
-    # Tenta encontrar base trivial, I em alguma parte de A, se não encontrar, retorna simplex 1 fase com variaveis auxiliares
-    pass  
-    
+def findTrivialbasis(A):
+    ''' Encontra colunas triviais para a base'''
+
+    basis = [ -1 for _ in range(len(A))]
+    A = handleZero(A)
+
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            if A[i][j] == 1 and np.count_nonzero(A[:, j]) == 1:
+                basis[i] = j
+                break
+
+    return basis
+
 
 
 def main():
@@ -239,50 +255,111 @@ def main():
                         help="Política de seleção de coluna. Valores válidos: largest, bland, smallest.")
     args = parser.parse_args()
 
+    global width, decimals, policy 
+    
     width = args.digits
     decimals = args.decimals
     policy = args.policy
 
-    A, b, c, non_neg, signs = read_simplex_input(args.filename)
+    A, b, c, non_neg, signs, num_variables, num_restrictions = readInput(args.filename)
 
+    # A, rowsEliminated = makeMatrixFullRank(A)
+    # print("Linhas eliminadas: ", rowsEliminated)
+    # b = np.delete(b, rowsEliminated)
+    
     # Transform to FPI    
-    
-    A, b, c, valopt, M, M_c = getFPI()
-    A, rowsEliminated = makeMatrixFullRank(A)
-    print('rowsEliminated =', rowsEliminated)
 
-    # Phase 1    
+    newColumns = 0
+    for j in range(len(non_neg)):
+        if non_neg[j] == 0: # Free variable
+            A = np.insert(A, A.shape[1], 0, axis=1)
+            A[:, -1] = -A[:, j]
+            c = np.insert(c, c.shape[0], -c[j])
+            # newColumns += 1
+        elif non_neg[j] == -1: # Non-positive variable
+            A[:, j] = -A[:, j]
+            c[j] = -c[j]
+
+
+    for i in range(len(A)):
+        if signs[i] == '<=':
+            A = np.insert(A, A.shape[1], 0, axis=1)
+            A[i][A.shape[1] - 1] = 1
+            c = np.insert(c, c.shape[0], 0)
+            newColumns += 1
+        elif signs[i] == '>=':
+            A = np.insert(A, A.shape[1], 0, axis=1)
+            A[i][A.shape[1] - 1] = -1
+            c = np.insert(c, c.shape[0], 0)
+            newColumns += 1
     
-    basis, A_aux, b, val_opt, M, M_c = first_phase(A, b, c, M, M_c)
-    print(val_opt)
+    M = np.eye(len(A))
+    M_c = np.zeros(len(A))
+    val_opt = 0.0
     
+    
+    for i, val in enumerate(b):
+        if val < 0:
+            A[i] = -A[i]
+            b[i] = -b[i]
+            M[i] = -M[i]
+    
+    
+    basis = findTrivialbasis(A)   
+    
+    c = -c
+    printTableau(A, b, c, val_opt, M, M_c, basis, "Tableau para a PL FPI: ")
+    
+    try:
+        if -1 in basis: # Temos que rodar com variaveis artificiais
+            basis, A, b, M, M_c = firstPhase(basis, A, b, M, M_c)
+            printTableau(A, b, c, val_opt, M, M_c, basis, "Tableau para a PL FPI, usando base encontrada: ")
+    
+    except Infeasible :
+        print("Status: Inviável")
+        return
+    
+    except Unbounded:
+        print("Status: Inviável")
+        return 
     
     # Phase 2 
-    
-    A = A_aux[:, :(A_aux.shape[1] - len(b))]
-    val_opt = canonicalForm(basis, A, b, c, 0, M, M_c)            
-    
-    print("Canonical form to PL after getting the basis for it: ")
-    printTableau(A, b, c, val_opt, M, M_c)
+    # Canonizar o tableau
+    for i, bi in enumerate(basis):
+        val_opt = pivot(A, b, c, val_opt, M, M_c, i, bi)
+                 
+    printTableau(A, b, c, val_opt, M, M_c, basis, "Forma canônica da PL: ")
     
     curIter = 0
     
-    while np.any(c < 0):
-        val_opt = simplexIteration(basis, A, b, c, val_opt, M, M_c)
-        print(f"Iteração {curIter}")
-        printTableau(A, b, c, val_opt, M, M_c)
-        curIter += 1
-        print("basis:", basis)
+    try:
+        while np.any(c < 0):
+            val_opt = simplexIteration(basis, A, b, c, val_opt, M, M_c)
+            curIter += 1
+            printTableau(A, b, c, val_opt, M, M_c, basis, f"Iteração {curIter}")
+            
+    except Unbounded:
+        print("Status: Ilimitada")
+        return
 
     sol = [0 for _ in range(len(c))]
     for row, col in enumerate(basis):
         sol[col] = b[row]
 
-    print("Solution to FPI PL: ")
 
+    sol = sol[:-newColumns]
+    
+    print("Status: otimo")
+    print("Objetivo: ", formatValue(val_opt, width, decimals))
+    print("Solução: ")
+    
     for i in sol:
         print(formatValue(i, width, decimals), end=' ')
     print()
     
+    print("Dual: ")
+    for i in M_c:
+        print(formatValue(i, width, decimals), end=' ')    
+    print()
         
 if __name__ == '__main__': main()
